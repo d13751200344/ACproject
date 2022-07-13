d0767d9aefc8cb690afbd30756c990fefe6827a0
Daniel <d13751200344@gmail.com>
2022-07-13 18:41:24 +0800

    signupDone

--- app.js
+++ app.js
@@ -13,9 +13,9 @@ const User = require("./models/user");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
-app.use(cookieParser(process.env.名稱));
+app.use(cookieParser(process.env.SECRET));
 app.use(session({
-	secret: process.env.名稱,
+	secret: process.env.SECRET,
 	resave: false,
 	saveUninitialized: true,
 }));
@@ -34,11 +34,6 @@ mongoose.connect("mongodb://localhost:27017/test", {
 	console.log(err);
 });
 
-
-app.get("/", (req, res) => {
-    res.render("index");
-})
-
 app.get("/", (req, res) => {
     res.send("Home page.");
 });
@@ -47,12 +42,46 @@ app.get("/signup", (req, res) => {
     res.render("signup");
 });
 
-app.post("/signup", (req, res) => {
+app.post("/signup", (req, res, next) => {
     //console.log(req.body);  終端機將顯示使用者輸入的帳密
     let { username, password } = req.body;
-    res.send("Thanks for posting.");
+	/* 下方接著創建newUser，assign它為新的User(引入的Schema)，其資料中的username與password等於上方從req.body所提取之值 */
+	let newUser = new User({ username: username, password: password});
+	try {
+		newUser.save()
+			.then(()=>{
+				res.send("Data has been saved.")
+			})
+			.catch((e)=>{
+				console.log(e);
+				res.send("Failed to save data.")
+			});
+	} catch(err) {
+		next(err);
+	}
 });
 //當使用者輸入帳密之後，按下送出＝在/signup中進行 post req，則系統將回以"Thanks for posting"
+
+app.get("/login", (req, res) => {
+	res.render("login");
+});
+app.post("/login", async (req, res, next) => {
+	let {username, password} = req.body;  //提取使用者輸入的帳密
+	try {
+		//接著在User model中尋找吻合的資料
+		let foundUser = await User.findOne({username: username});
+		//接著用if statement確認資料。
+		//補充: if(!foundUser) 代表找不到foundUser
+		//如果有找到foundUser且密碼沒錯則看到secret；找不到foundUser或密碼不對則進else
+		if (foundUser && password == foundUser.password) {
+			res.render("secret");
+		} else {
+			res.send("Incorrect username or password.")
+		}
+	} catch (err) {
+		next(err);
+	}
+});
 
 app.get("/*", (req, res) => {
 	res.status(404).send("The page you're searching for doesn't exist.");
--- models/user.js
+++ models/user.js
@@ -3,15 +3,15 @@ const mongoose = require("mongoose");
 //製作一個新的資料架構 userSchema
 const userSchema = new mongoose.Schema({
     username:{
-        type: string,
+        type: String,
     },
     password:{
-        type: string,
+        type: String,
     },
 });
 
 // model宣告：將 User 變數 assign 為資料架構 userSchema
-const User = mongoose.model("User", "userSchema");
+const User = mongoose.model("User", userSchema);
 
 // 將 User 變數輸出
 module.exports = User;
